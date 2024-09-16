9691437b2f33fde3f4e66c66ea79e02a05002e5f
Carlos Farinas <cfarinas@gmail.com>
2024-09-16 10:23:23 -0400

    Add project files.

---
+++ Data/BlogDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web.Blog.Models.Domain;
+
+namespace Web.Blog.Data
+{
+    public class BlogDBContext : DbContext
+    {
+        public BlogDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPost { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
